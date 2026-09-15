4ece3a1fab6d994e1e91f4b8f5979996cb001ee5
joao <sonicjoa06@gmail.com>
2026-09-15 16:35:09 -0300

    fix:arrumando erros do banco de dados

--- AulaCrud/Data/DataBase.cs
+++ AulaCrud/Data/DataBase.cs
@@ -4,8 +4,7 @@ namespace AulaCrud.Data
 {
     public class DataBase
     {
-        private readonly string connectionString = "server=localhost;port=3306;database=Oficina;user=root;password=12345678;";
-
+        private readonly string connectionString = "server=localhost;port=3305;database=Oficina_db;user=root;password=12345678;";
         public MySqlConnection GetConnection()
         {
             MySqlConnection conn = new MySqlConnection(connectionString);
